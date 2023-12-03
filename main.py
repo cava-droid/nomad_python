a1c649a7d76546bf2909f88c1f01beb911321ba2
cavaMetro <cava@seoulmetro.co.kr>
2023-12-04 06:37:23 +0900

    2.2 Booleans ans Strings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 2.1 Variables
+# 2.2 Booleans ans Strings
 
 # python은 snake_case를 기본적으로 사용
 
@@ -8,3 +8,10 @@ b = 3
 c = a + b
 
 print(c)
+
+my_name = "nico"
+print(my_name)
+
+dead = False
+# dead = "False"는 boolean이 아니라 string
+# false 아님을 주의
