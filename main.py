6cc2b573f6c55f1e2f71cc587e15832d695d0ca5
cava <cava@kaist.ac.kr>
2024-02-24 11:33:42 +0900

    4.5 For Loops

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-# 2.2 Booleans ans Strings
+# 4.5 For Loops
 
-# python은 snake_case를 기본적으로 사용
+websites = (
+    "google.com",
+    "airbnb.com",
+    "twitter.com",
+    "facebook.com",
+    "tiktok.com",
+)
 
-
-a = 2
-b = 3
-c = a + b
-
-print(c)
-
-my_name = "nico"
-print(my_name)
-
-dead = False
-# dead = "False"는 boolean이 아니라 string
-# false 아님을 주의
+for website in websites:
+    print(website)
