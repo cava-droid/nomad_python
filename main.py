64b3bcc1a50b51c04406bcc777d71c2f137ab8e5
cavaMetro <cava@seoulmetro.co.kr>
2024-03-03 08:05:18 +0900

    6.2 BeautifulSoup 회사여서 안 됨;;;

--- main.py
+++ main.py
@@ -1,47 +1,7 @@
-# 5.7 Code Challenge
+# 6.2 BeautifulSoup
 
+import requests
 
-class Player:
-    def __init__(self, name, team):
-        self.name = name
-        self.xp = 1500
-        self.team = team
+url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
-    def introduce(self):
-        print(f"Hello! I'm {self.name} and I play for {self.team}")
-
-
-class Team:
-    def __init__(self, team_name):
-        self.team_name = team_name
-        self.players = []
-
-    def add_player(self, name):
-        new_player = Player(name, self.team_name)
-        self.players.append(new_player)
-
-    def show_players(self):
-        for player in self.players:
-            player.introduce()
-
-
-team_x = Team("Team X")
-team_blue = Team("Team Blue")
-
-
-# nico = Player(
-#     name="nico",
-#     team="Team X",
-# )
-
-# lynn = Player(
-#     name="lynn",
-#     team="Team Blue",
-# )
-
-team_x.add_player("nico")
-# team_blue = team_blue.add_player("Lynn")  이렇게 쓰면 에러;;;
-team_blue.add_player("Lynn")
-
-team_x.show_players()
-team_blue.show_players()
+response = requests.get(url)
