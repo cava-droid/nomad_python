c3b3c62c8cc36f733c28c2d6f26d5795dee5f5d1
cava <cava@kaist.ac.kr>
2024-03-03 11:53:13 +0900

    6.2 BeautifulSoup

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 # 6.2 BeautifulSoup
 
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
 
 response = requests.get(url)
+
+soup = BeautifulSoup(
+    response.content,
+    "html.parser",
+)
+
+# jobs = soup.find("section", id="category-2")
+
+# class는 예약어이므로 class_로 사용
+jobs = soup.find("section", class_="jobs").find_all("li")
+
+print(jobs)
