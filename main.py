2df5b083b8ee4ccc17c57f3f8e1f740290596474
cava <cava@kaist.ac.kr>
2024-03-14 15:48:09 +0900

    6.3 Jobs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 6.2 BeautifulSoup
+# 6.3 Jobs
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,6 +15,15 @@ soup = BeautifulSoup(
 # jobs = soup.find("section", id="category-2")
 
 # class는 예약어이므로 class_로 사용
-jobs = soup.find("section", class_="jobs").find_all("li")
+# [1:-1]로 첫번째 항목과 마지막 항목 삭제
+jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
-print(jobs)
+# text만 추출하려면 find()뒤에 .text 추가
+for job in jobs:
+    title = job.find("span", class_="title").text
+    # region = job.find("span", class_="region").text
+    company, position, region = job.find_all("span", class_="company")
+    company = company.text
+    position = position.text
+    region = region.text
+    print(title, company, position, region, "------------------\n")
