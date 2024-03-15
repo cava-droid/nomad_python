1e4c9f28fe2b44f04ccb648ccc32ab44f03782cc
cava <cava@kaist.ac.kr>
2024-03-15 13:59:03 +0900

    6.4 Recap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 6.3 Jobs
+# 6.4 Recap
 
 import requests
 from bs4 import BeautifulSoup
@@ -18,12 +18,26 @@ soup = BeautifulSoup(
 # [1:-1]로 첫번째 항목과 마지막 항목 삭제
 jobs = soup.find("section", class_="jobs").find_all("li")[1:-1]
 
+all_jobs = []
+
 # text만 추출하려면 find()뒤에 .text 추가
 for job in jobs:
     title = job.find("span", class_="title").text
     # region = job.find("span", class_="region").text
     company, position, region = job.find_all("span", class_="company")
-    company = company.text
-    position = position.text
-    region = region.text
-    print(title, company, position, region, "------------------\n")
+    url = job.find("div", class_="tooltip--flag-logo").next_sibling["href"]
+    # company = company.text
+    # position = position.text
+    # region = region.text
+    # print(title, company, position, region, "------------------\n")
+    job_data = {
+        "title": title,
+        "company": company.text,
+        "position": position.text,
+        "region": region.text,
+        "url": f"https://weworkremotely.com{url}",
+    }
+    all_jobs.append(job_data)
+
+
+print(all_jobs)
