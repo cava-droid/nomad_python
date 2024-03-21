289eebaa888220e75d45a5cdfe6a4bbb18006492
cava <cava@kaist.ac.kr>
2024-03-21 15:22:26 +0900

    6.10 Interactivity

--- dynamic_scraper/main.py
+++ dynamic_scraper/main.py
@@ -1,6 +1,8 @@
-# 6.8 PlayWright
+# 6.10 Interactivity
 
 from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
 
 p = sync_playwright().start()
 
@@ -12,8 +14,34 @@ browser = p.chromium.launch(headless=False)
 # 새로운 탭 열기
 page = browser.new_page()
 
-# 구글사이트로 이동
-page.goto("https://google.com")
+# 사이트로 이동
+page.goto("https://www.wanted.co.kr/jobsfeed")
 
-# 페이지 스크린샷 저장
-page.screenshot(path="./dynamic_scraper/screenshot.png")
+time.sleep(3)
+
+page.click("button.Aside_searchButton__Xhqq3")
+# page.locator("button.Aside_searchButton__Xhqq3").click()
+
+time.sleep(3)
+
+page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
+
+time.sleep(3)
+
+page.keyboard.down("Enter")
+
+time.sleep(5)
+
+page.click("a#search_tab_position")
+
+time.sleep(5)
+
+for x in range(4):
+    page.keyboard.down("End")
+    time.sleep(5)
+
+content = page.content()
+
+p.stop()
+
+soup = BeautifulSoup(content, "html.parser")
