9f728c7895ec64dd8fc1903ad693f0db653416ed
cava <cava@kaist.ac.kr>
2024-03-21 15:24:14 +0900

    6.10 Interactivity2

--- dynamic_scraper/main.py
+++ dynamic_scraper/main.py
@@ -1,4 +1,4 @@
-# 6.10 Interactivity
+# 6.11 Collecting Jobs
 
 from playwright.sync_api import sync_playwright
 import time
@@ -19,23 +19,28 @@ page.goto("https://www.wanted.co.kr/jobsfeed")
 
 time.sleep(3)
 
+# class명으로 선택하여 클릭
 page.click("button.Aside_searchButton__Xhqq3")
 # page.locator("button.Aside_searchButton__Xhqq3").click()
 
 time.sleep(3)
 
+# placeholder 내용으로 선택하여 빈칸에 내용 채움
 page.get_by_placeholder("검색어를 입력해 주세요.").fill("flutter")
 
 time.sleep(3)
 
+# Enter 키를 누른 효과
 page.keyboard.down("Enter")
 
 time.sleep(5)
 
+# id명으로 선택하여 클릭
 page.click("a#search_tab_position")
 
 time.sleep(5)
 
+# 페이지 아래로 이동
 for x in range(4):
     page.keyboard.down("End")
     time.sleep(5)
