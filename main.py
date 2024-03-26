147be77653f3dd34552f9daed0ae11ba78f30f35
cava <cava@kaist.ac.kr>
2024-03-26 15:32:18 +0900

    111

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # 7.8 Cache
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from dynamic_scraper.job_scraper import scrap_jobs
+from file import save_to_file
 
 app = Flask("JobScrapper")
 
@@ -16,12 +17,24 @@ def home():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
     if keyword in db:
         jobs = db[keyword]
     else:
         jobs = scrap_jobs(keyword)
         db[keyword] = jobs
     return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+    if keyword == None:
+        return redirect("/")
+    if keyword is not db:
+        return redirect(f"/search?keyword={keyword}")
+    save_to_file(keyword, db[keyword])
 
 
 app.run("127.0.0.1", debug=True)
