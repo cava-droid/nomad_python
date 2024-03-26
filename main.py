d532af76ed9bedcc12e2a3addb7dbd843e693894
cava <cava@kaist.ac.kr>
2024-03-26 11:34:07 +0900

    7.5 Arguments

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-# 7.3 Form
+# 7.5 Arguments
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("JobScrapper")
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", name="nico")
+    return render_template("home.html")
 
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    return render_template("search.html", keyword=keyword)
 
 
 app.run("127.0.0.1", debug=True)
